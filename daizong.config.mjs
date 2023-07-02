da85d4b1eb2ead6c437d1c4954581277ba637605
Janka Uryga <Janka.Uryga@stepstone.com>
2023-07-02 03:02:51 +0200

    increase test timeout

--- daizong.config.mjs
+++ daizong.config.mjs
@@ -10,7 +10,7 @@ export default {
     before: {
       del: './tests/dist',
     },
-    run: 'mocha --parallel --require source-map-support/register dist_tests/**/*.test.js --timeout 10000',
+    run: 'mocha --parallel --require source-map-support/register dist_tests/**/*.test.js --timeout 20000',
   },
 
   // Cleans, lints, compiles sources and runs tests.
